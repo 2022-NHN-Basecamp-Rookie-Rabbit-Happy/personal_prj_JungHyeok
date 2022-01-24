84886f41dd9971c86817e11728e9ea9cad950b77
JungHyeokMoon <wjdgur9534@daum.net>
2022-01-24 13:16:12 +0900

    filter example 주석

---
+++ src/main/java/com/example/demo/filter/ResponseWrapFilter.java
@@ -0,0 +1,32 @@
+//package com.example.demo.filter;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@Component
+//public class ResponseWrapFilter implements Filter {
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        Filter.super.init(filterConfig);
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//
+//        HttpServletRequest req = (HttpServletRequest) request;
+//        HttpServletResponse res = (HttpServletResponse) response;
+//
+//        res.setStatus(HttpStatus.OK.value());
+//        chain.doFilter(req,res);
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        Filter.super.destroy();
+//    }
+//}
